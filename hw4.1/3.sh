bcd4494c4b2a783d2ba32cfe73785506dee7e77b
EcRiptor <ecriptor@gmail.com>
2020-08-31 19:37:44 +0300

    edit script 4.sh

--- hw4.1/3.sh
+++ hw4.1/3.sh
@@ -3,7 +3,7 @@ ip=('192.168.1.1' '173.194.222.113' '87.250.250.242')
 var1=5
 while [ $var1 -gt 0 ]
 do
-	for i in ${ip[@]}
+	for i in "${ip[@]}"
 	do
 		echo $i >> log
 		curl -s -w '%{http_code}\n' $i:80 -o /dev/null >> log
--- hw4.1/4.sh
+++ hw4.1/4.sh
@@ -1,19 +1,16 @@
 ip=('173.194.222.113' '87.250.250.242' '192.168.0.1')
-set -x
-var1=5
-while [ $var1 -gt 0 ]
+#set -x
+while :
 do
-	for i in ${ip[@]}
+	for i in "${ip[@]}"
 	do
 		echo $i >> log
 		curl -s -w '%{http_code}\n' $i:80 -o /dev/null >> log
 		if [[ $? == 0 ]]
 		then
 			continue
-		else
+		fi
 			echo $i > error
 			break 2
-		fi
 	done
-	var1=$[ $var1 - 1 ]
 done
