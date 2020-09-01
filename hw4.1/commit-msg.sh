cdd0401d900bedf867a021bc589c2089b5bf4983
EcRiptor <ecriptor@gmail.com>
2020-09-01 16:43:08 +0300

    [04-l-05-done] commit-msg

--- hw4.1/commit-msg.sh
+++ hw4.1/commit-msg.sh
@@ -1,18 +1,7 @@
 #!/bin/bash 
-#Скрипт /devops-netology/.git/hooks/commit-msg
-#!/usr/bin/env bash
-#while read line
-#do
-#       	# пропускаем строки комментариев	
-#	if [ "${line:0:1}" == "#" ]
-#	then
-#		continue
-#	fi
-#done < "${1}"
-#commit_regex='^\[04-script-01-bash\]'
-commit_regex='^(\[[0-9]{2}\-[a-zA-Z]+\-[0-9]{2}\-[a-z]+\])([a-z\ \-]{1,30})'
-error_msg="Сообщение коммита должно начинаться с [04-script-01-bash]"
-#если в сообщении коммита нет строки, начинающейся с нужного шаблона
+#Скрипт расположен в директории /<репозиторий>/.git/hooks/commit-msg
+commit_regex='^(\[[0-9]{2}\-[a-zA-Z]+\-[0-9]{2}\-[a-z]+\])([a-zA-Zа-яА-Я\ \-\,\.\:\;]{1,30})'
+error_msg='Сообщение коммита должно иметь следующий вид "[01-script-01-bash] messages" (messages не больше 30 символов)'
 if ! grep -iqE "$commit_regex" "$1"; then
 	echo "$error_msg" >&2
 	exit 1
