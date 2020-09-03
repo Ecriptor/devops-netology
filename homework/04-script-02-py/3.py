42c248018191369e875197b210655708c4028803
EcRiptor <ecriptor@gmail.com>
2020-09-03 16:11:41 +0300

    edit py

--- homework/04-script-02-py/3.py
+++ homework/04-script-02-py/3.py
@@ -1,28 +1,5 @@
-# 04-script-02-py
-1. Ex1
-* Будет ошибка из-за несовпадения типов
-* Нужно переменную a задать в кавычках  **a = '2'** или при присвоении c = str(a) + str(b) 
-* Нужно у переменной b убрать кавычки  **b = 2** или при присвоении c = int(a) + int(b)
-2. [Ex2](2.py)
-```python
 #!/usr/bin/env python3
-  
-import os
 
-git_repo_dir = '/home/ecriptor/devops-netology/'
-bash_command = ["cd "+git_repo_dir, "git status"]
-result_os = os.popen(' && '.join(bash_command)).read()
-prepare_result = ""
-for result in result_os.split('\n'):
-    if result.find('изменено') != -1:
-        prepare_result = prepare_result + git_repo_dir + result.replace('\tизменено:      ', '') + '\n'
-print(prepare_result)
-```
-3. [Ex3](3.py)
-
-```python
-#!/usr/bin/env python3
-  
 import os
 import sys
 
@@ -44,6 +21,3 @@ if result_os != "" and result_os.find("not a git repository") == -1:
 # Если строка "not a git repository" присутствовала, то выводим сообщение.
 else:
     print(f"{git_repo_dir} не является git репозиторием")
-```
-4. [Ex4]()
- 
