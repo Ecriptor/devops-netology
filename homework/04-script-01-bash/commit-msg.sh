4ffa4ea988aee3ff232f66ffd82eeb4e1bf106e1
EcRiptor <ecriptor@gmail.com>
2020-09-03 10:46:57 +0300

    edit commit-msg.sh

--- homework/04-script-01-bash/commit-msg.sh
+++ homework/04-script-01-bash/commit-msg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #Скрипт расположен в директории /<репозиторий>/.git/hooks/commit-msg
-commit_regex='^(\[[0-9]{2}\-[a-zA-Z]+\-[0-9]{2}\-[a-z]+\])([a-zA-Zа-яА-Я\ \-\,\.\:\;]{1,30})'
+commit_regex='^(\[[0-9]{2}\-[a-zA-Z]+\-[0-9]{2}\-[a-z]+\])([a-zA-Zа-яА-Я\ \-\,\.\:\;\(\)\_\!]{1,30})'
 error_msg='Сообщение коммита должно иметь следующий вид "[01-script-01-bash] messages" (messages не больше 30 символов)'
 if ! grep -iqE "$commit_regex" "$1"; then
 	echo "$error_msg" >&2
