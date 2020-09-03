74b36467fcc053c85c36f4202b8fc2bfb592e274
EcRiptor <ecriptor@gmail.com>
2020-09-03 16:11:41 +0300

    edit py

--- homework/04-script-02-py/2.py
+++ homework/04-script-02-py/2.py
@@ -5,9 +5,9 @@ import os
 git_repo_dir = '/home/ecriptor/devops-netology/'
 bash_command = ["cd "+git_repo_dir, "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
-is_change = False
+#is_change = False
+prepare_result = ""
 for result in result_os.split('\n'):
     if result.find('изменено') != -1:
-        prepare_result = git_repo_dir + result.replace('\tизменено:      ', '')
-        print(prepare_result)
-#        break
+        prepare_result = prepare_result + git_repo_dir + result.replace('\tизменено:      ', '') + '\n'
+print(prepare_result)
